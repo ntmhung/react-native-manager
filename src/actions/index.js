fde92a78b8b823e6fe6b4b957b5a12c909c9f730
Hung.Nguyen <ntmhung94@gmail.com>
2018-06-10 23:49:50 +0700

    async authentication using redux-thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 /**
  * Created by minhhung on 6/9/18.
  */
+import firebase from "firebase";
 import {EMAIL_CHANGED, PASSWORD_CHANGED} from "./types";
 
 export const emailChanged = (email) => {
@@ -11,9 +12,25 @@ export const emailChanged = (email) => {
 };
 
 export const passwordChanged = (password) => {
+    //get passed to dispatch()
     return {
         type: PASSWORD_CHANGED,
         payload: password
     };
 };
 
+/*
+ * Asynchronous action creator. Only dispatch actions to reducer when something done, such as AJAX call.
+ * Use redux-thunk
+ */
+export const loginUser = ({email, password}) => {
+    return (dispatch) => {
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then(user => {
+                dispatch({
+                    type: 'LOGIN_USER_SUCCESS',
+                    payload: user
+                })
+            });
+    };
+};
